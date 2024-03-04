3dbfd0d639cc44d2b83e81ca24eb156c05ab888e
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2024-03-04 19:32:28 -0300

    odakfofakd

---
+++ Modulo 2 - javascript/Clase7/Clase7.js
@@ -0,0 +1,18 @@
+// Objeto
+
+ // Objeto es un tipo de dato no primitivo
+ let objeto = {
+    //TUPLAS
+    // DUO DE CLAVE : VALOR
+    milanesa: "rica",
+    milanesa2: "rica",
+    milanesa3: "rica",
+    milanesa4: "rica",
+    milanesa5: "rica",
+    valorNumerico: 3431,
+    valorBoolean: true,
+    valorFunc: () => {},
+    valorObj: {
+        otraClave: "otraClave"
+    }
+ }
